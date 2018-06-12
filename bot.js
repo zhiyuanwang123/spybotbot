51a2c2a8a65d08df2239bc33459be99da303d81e
zhiyuanwang123 <39817104+zhiyuanwang123@users.noreply.github.com>
2018-06-11 22:47:36 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,10 @@
+const botsetting = require("./botsetting.json");
+const words = require("./wordspackage.json");
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+const fs = require("fs");
+client.currentgame = require("./currentgame.json");
 
 console.log(new Date().toLocaleTimeString());
 client.on('ready', async () => {
@@ -13,59 +17,95 @@ client.on('ready', async () => {
   }
 });
 
-client.on('message', msg => {
+client.on('message',async (msg) => {
   let msgarray = msg.content.split(" ");
   let command = msgarray[0];
   let args = msgarray.slice(1)
 
   if (msg.author.bot) return;
+  if (msg.channel.type === "dm"){
+    msg.channel.send("Developing DM!");
+    return;
+  }
+  if (msg.channel.type !== "dm"){
+    if (msg.content === 'game') {
+      msg.channel.send('Send [Create Room] to Create a game');
+    }
 
-  if (msg.content === 'ping') {
-    msg.channel.sendMessage("Hi, Wanna play Who is Undercover?");
-  }
-  if (msg.content === 'cennie') {
-    msg.channel.sendMessage("She is sooo stupid");
-  }
-  if (command === "!mute") {
+    if (msg.content.toLowerCase() === "create game" || msg.content.toLowerCase() === "creategame"){
+      client.currentgame[msg.guild.id] = {
+        player: msg.author.id,
+        playername: msg.author.username,
+        place: "2",
+      }
+      fs.writeFile("./currentgame.json", JSON.stringify(client.currentgame,null,4)), err => {
+        if(err) throw err;
+      }
+      let embed = new Discord.RichEmbed()
+        .setTitle("--Players in the game--")
+        .setDescription("Send [Join] to join the game\n--------------------------------")
+        .addField("Player 1", msg.author.username)
+        .setColor([0,120,255])
+      msg.channel.send(embed)
+    }
+
+    if (msg.content.toLowerCase() === "show room" || msg.content.toLowerCase() === "showroom"){
+      let playerinfos = client.currentgame[msg.guild.id].player;
+      msg.channel.send(playerinfos);
+    }
+    if (msg.content.toLowerCase().search("cennie") >= 0) {
+      msg.author.send("Stop saying Cennie!");
+    }
+  
+    if (command === "!mute") {
+      if (!msg.member.hasPermission("MANAGE_MESSAGES")) return msg.reply("You don't have permissions");
+      let mentionuser =msg.mentions.users.first();
+      if (!mentionuser) return msg.reply("Please specify a user");
+      let tomute = msg.guild.members.get(mentionuser.id) || msg.guild.members.get(args[0]);
+      if (!tomute) return msg.reply("Please specify a user");
+      msg.channel.overwritePermissions(tomute, {
+        SEND_MESSAGES: false,
+      })
+      return msg.reply(`${tomute.user.username} is muted`);
+    }
+  
+    if (command === "!unmute") {
+      if (!msg.member.hasPermission("MANAGE_MESSAGES")) return msg.reply("You don't have permissions");
+      let mentionuser =msg.mentions.users.first();
+      if (!mentionuser) return msg.reply("Please specify a user");
+      let tomute = msg.guild.members.get(mentionuser.id) || msg.guild.members.get(args[0]);
+      if (!tomute) return msg.reply("Please specify a user");
+      msg.channel.overwritePermissions(tomute, {
+        SEND_MESSAGES: null,
+      })
+      return msg.reply(`${tomute.user.username} is unmuted`);
+    }
+  
+  if (command === "!cover") {
     if (!msg.member.hasPermission("MANAGE_MESSAGES")) return msg.reply("You don't have permissions");
-    let tomute = msg.guild.members.get(msg.mentions.users.first().id) || msg.guild.members.get(args[0]);
-    console.log(tomute);
+    let mentionuser =msg.mentions.users.first();
+    if (!mentionuser) return msg.reply("Please specify a user");
+    let tomute = msg.guild.members.get(mentionuser.id) || msg.guild.members.get(args[0]);
     if (!tomute) return msg.reply("Please specify a user");
     msg.channel.overwritePermissions(tomute, {
-      SEND_MESSAGES: false,
+      READ_MESSAGES: false,
     })
-    return msg.reply(`${tomute.user.username} is muted`);
+    return msg.reply(`${tomute.user.username} is covered`);
   }
-
-  if (command === "!unmute") {
-    if (!msg.member.hasPermission("MANAGE_MESSAGES")) return msg.reply("You don't have permissions");
-    let tomute = msg.guild.members.get(msg.mentions.users.first().id) || msg.guild.members.get(args[0]);
-    if (!tomute) return msg.reply("Please specify a user");
-    msg.channel.overwritePermissions(tomute, {
-      SEND_MESSAGES: null,
-    })
-    return msg.reply(`${tomute.user.username} is unmuted`);
-  }
-
-if (command === "!cover") {
-  if (!msg.member.hasPermission("MANAGE_MESSAGES")) return msg.reply("You don't have permissions");
-  let tomute = msg.guild.members.get(msg.mentions.users.first().id) || msg.guild.members.get(args[0]);
-  if (!tomute) return msg.reply("Please specify a user");
-  msg.channel.overwritePermissions(tomute, {
-    READ_MESSAGES: false,
-  })
-  return msg.reply(`${tomute.user.username} is covered`);
-}
-
-  if (command === "!uncover") {
-    if (!msg.member.hasPermission("MANAGE_MESSAGES")) return msg.reply("You don't have permissions");
-    let tomute = msg.guild.members.get(msg.mentions.users.first().id) || msg.guild.members.get(args[0]);
-    if (!tomute) return msg.reply("Please specify a user");
-    msg.channel.overwritePermissions(tomute, {
-      READ_MESSAGES: null,
-    })
-    return msg.reply(`${tomute.user.username} is uncovered`);
-  }
-  return
+  
+    if (command === "!uncover") {
+      if (!msg.member.hasPermission("MANAGE_MESSAGES")) return msg.reply("You don't have permissions");
+      let mentionuser =msg.mentions.users.first();
+      if (!mentionuser) return msg.reply("Please specify a user (Works On PC)");
+      let tomute = msg.guild.members.get(mentionuser.id) || msg.guild.members.get(args[0]);
+      if (!tomute) return msg.reply("Please specify a user (Works On PC)");
+      msg.channel.overwritePermissions(tomute, {
+        READ_MESSAGES: null,
+      })
+      return msg.reply(`${tomute.user.username} is uncovered`);
+    }
+    return;
+  } 
+ 
 });
 client.login(process.env.BOT_TOKEN);
